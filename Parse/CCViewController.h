e29545ef80d0f7b78db912baff3528aef0f78d0a
Eliot Arntz <arntzel@gmail.com>
2013-08-13 14:43:33 -0400

    Initial Commit

---
+++ Parse/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Parse
+//
+//  Created by Eliot Arntz on 8/13/13.
+//  Copyright (c) 2013 Eliot Arntz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Parse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Parse
+//
+//  Created by Eliot Arntz on 8/13/13.
+//  Copyright (c) 2013 Eliot Arntz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
